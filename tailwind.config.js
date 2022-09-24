82674252e59830bcb0cafe7b3d1f97758f71f917
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2022-09-24 15:19:09 +0200

    fix hot reload issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 const { withTheme } = require("@op-ent/unstyled-ui-theme");
 
-// TODO: investigate hot reloading issue with components
-
 /** @type {import('tailwindcss').Config} */
 module.exports = withTheme({
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@op-ent/unstyled-ui/dist/index.js",
   ],
   theme: {
     extend: {},
